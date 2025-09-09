0e8db8f65a3b3e8d7f180cc0b6e507b4bd1cb41c
绎凡 金 <18107907861@163.com>
2025-09-09 16:18:39 +0800

    init main

--- src/main.cpp
+++ src/main.cpp
@@ -1,115 +1,210 @@
-//
-// Created by PinkySmile on 31/10/2020
-//
-
+//thanks to @Co1umbium and @fishshapedapple
 #include <SokuLib.hpp>
 #include <Shlwapi.h>
 #pragma comment(lib, "Shlwapi.lib")
+#pragma comment(lib, "imm32.lib")
+#include <dinput.h>
 
-static bool init = false;
-static int (SokuLib::BattleManager::*ogBattleMgrOnProcess)();
-static void (SokuLib::BattleManager::*ogBattleMgrOnRender)();
-static SokuLib::DrawUtils::Sprite text;
-static SokuLib::SWRFont font;
-
-int __fastcall CBattleManager_OnRender(SokuLib::BattleManager *This)
-{
-	(This->*ogBattleMgrOnRender)();
-	text.draw();
-	return 0;
-}
-
-void loadFont()
-{
-	SokuLib::FontDescription desc;
-
-	// Pink
-	desc.r1 = 255;
-	desc.g1 = 155;
-	desc.b1 = 155;
-	// Light green
-	desc.r2 = 155;
-	desc.g2 = 255;
-	desc.b2 = 155;
-	desc.height = 24;
-	desc.weight = FW_BOLD;
-	desc.italic = 0;
-	desc.shadow = 4;
-	desc.bufferSize = 1000000;
-	desc.charSpaceX = 0;
-	desc.charSpaceY = 0;
-	desc.offsetX = 0;
-	desc.offsetY = 0;
-	desc.useOffset = 0;
-	strcpy(desc.faceName, "MonoSpatialModSWR");
-	font.create();
-	font.setIndirect(desc);
-}
-
-int __fastcall CBattleManager_OnProcess(SokuLib::BattleManager *This)
-{
-	if (!init) {
-		SokuLib::Vector2i realSize;
-
-		loadFont();
-		text.texture.createFromText("Hello, world!", font, {300, 300}, &realSize);
-		text.setPosition(SokuLib::Vector2i{320 - realSize.x / 2, 240 - realSize.y / 2});
-		text.setSize(realSize.to<unsigned>());
-		text.rect.width = realSize.x;
-		text.rect.height = realSize.y;
-		init = true;
+template<typename T>
+static T* TamperIndirectCall(DWORD addr, T* target) {
+	DWORD old;
+	T* org = NULL;
+	auto& address = *reinterpret_cast<unsigned char**>(&addr);
+	VirtualProtect(address, 6, PAGE_EXECUTE_WRITECOPY, &old);
+	if (*address == 0xFF && address[1] == 0x15) {//rel32 call
+		DWORD old2;
+		T** &addr2 = *reinterpret_cast<T***>(address + 2);
+		VirtualProtect(addr2, 4, PAGE_READONLY, &old2);
+		org = *addr2;
+		VirtualProtect(addr2, 4, old2, &old2);
+		unsigned char call[] = { 0xE8, 0, 0, 0, 0, 0x90 };
+		*(DWORD*)(call + 1) = DWORD(target)-DWORD(address + 5);
+		memcpy(address, call, sizeof(call));
+	} else if (*address == 0xE8) {//near call
+		SokuLib::TamperNearCall(addr, target);
+	} else {
+		// unknown
 	}
-	text.setRotation(text.getRotation() + 0.01f);
-	return (This->*ogBattleMgrOnProcess)();
+	VirtualProtect(address, 6, old, &old);
+	return org;
 }
 
-// We check if the game version is what we target (in our case, Soku 1.10a).
-extern "C" __declspec(dllexport) bool CheckVersion(const BYTE hash[16])
+/*HHOOK g_hHook = NULL;
+HINSTANCE g_hDll = NULL;
+LRESULT CALLBACK LowLevelKeyboardProc(int nCode, WPARAM wParam, LPARAM lParam)//ÆÁ±Î Win ¼üµ¥»÷
 {
-	return memcmp(hash, SokuLib::targetHash, sizeof(SokuLib::targetHash)) == 0;
+	if (nCode == HC_ACTION 
+		&& GetForegroundWindow() == SokuLib::window
+	) {
+		KBDLLHOOKSTRUCT* p = (KBDLLHOOKSTRUCT*)lParam;
+		// ÆÁ±Î Win ¼ü
+		if (p->vkCode == VK_LWIN || p->vkCode == VK_RWIN) {
+			if (wParam == WM_KEYUP || wParam == WM_SYSKEYUP) {
+				printf("WH_KEYBOARD_LL: Win key up blocked\n");
+				return 1; // ·µ»Ø·ÇÁã£¬³ÔµôÏûÏ¢
+			}
+		}
+		// ÆÁ±Î Alt+Space£¨Õë¶Ôpowertoys£¿£©
+		
+	}
+	return CallNextHookEx(g_hHook, nCode, wParam, lParam);
+}
+inline void HookInstall()
+{
+	if (g_hHook) {
+		return;
+	}
+	g_hHook = SetWindowsHookEx(WH_KEYBOARD_LL, LowLevelKeyboardProc, g_hDll, 0);//really wierd workaround with AltDrag.exe
 }
 
-// Called when the mod loader is ready to initialize this module.
-// All hooks should be placed here. It's also a good moment to load settings from the ini.
+inline void HookRemove()
+{
+	if (g_hHook) {
+		UnhookWindowsHookEx(g_hHook);
+		g_hHook = NULL;
+	}
+}*/
+
+void FakePress()
+{
+	INPUT input[2] = { 0 };
+
+	// Ctrl down
+	input[0].type = INPUT_KEYBOARD;
+	input[0].ki.wVk = VK_MENU;
+	input[0].ki.dwFlags = 0;
+
+	// Ctrl up
+	input[1].type = INPUT_KEYBOARD;
+	input[1].ki.wVk = VK_MENU;
+	input[1].ki.dwFlags = KEYEVENTF_KEYUP;
+
+	SendInput(2, input, sizeof(INPUT));
+}
+
+
+static WNDPROC originalWndProc = nullptr;
+static LRESULT CALLBACK PatchedWndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+	unsigned short key = LOWORD(wParam);
+	switch (uMsg) {
+	case WM_IME_SETCONTEXT:
+
+		break;
+
+	case WM_KEYDOWN:
+		if ((key == VK_LWIN || key == VK_RWIN)
+		&& !(HIWORD(lParam) & KF_REPEAT)//preiviously not pressed
+		) {
+			FakePress();
+			puts("WIN UP blocked");
+			return 0;
+		}
+		break;
+	case WM_SYSCOMMAND:
+		if (key == SC_KEYMENU || key == SC_TASKLIST) {
+			puts("WM_SYSCOMMAND blocked");
+			return 0;
+		}
+		break;
+	case WM_DESTROY:
+		//HookRemove();
+		break;
+	}
+
+
+	return CallWindowProc(originalWndProc, hWnd, uMsg, wParam, lParam);
+}
+
+
+
+inline void setEngIME(HWND hwnd) {
+	MAKELANGID(LANG_ENGLISH, SUBLANG_ENGLISH_US);
+	HKL hkl = LoadKeyboardLayoutW(L"00000409", KLF_ACTIVATE);
+	if (hkl) {
+		ActivateKeyboardLayout(hkl, KLF_SETFORPROCESS);
+		puts("KeyboradLayout switched to EN-US. ");
+	}
+	else {
+		HIMC hIMC = ImmGetContext(hwnd);
+		if (hIMC) {
+			ImmSetOpenStatus(hIMC, FALSE);
+			ImmReleaseContext(hwnd, hIMC);
+			puts("Tried to close Input Method. ");
+		}
+		else {
+
+		}
+	}
+}
+
+
+static BOOL(__stdcall* orgUpdateWindow)(HWND) = NULL;
+static BOOL __stdcall afterWndCreated(HWND hwnd) {
+	auto ret = orgUpdateWindow(hwnd);
+	originalWndProc = (WNDPROC)GetWindowLongPtr(hwnd, GWLP_WNDPROC);
+	if (originalWndProc)
+		SetWindowLongPtr(hwnd, GWLP_WNDPROC, (LONG_PTR)PatchedWndProc);
+
+	setEngIME(hwnd);
+
+	return ret;
+}
+//typedef int(__thiscall SokuLib::Logo::* pfLogoIV)();
+//static void __cdecl afterWindowStart(void* _ignored)
+//{
+//	HWND hGameWnd = nullptr;
+//	while (!hGameWnd) {
+//		hGameWnd = SokuLib::window;
+//		Sleep(200);
+//		puts("Thread tried");
+//	}
+//	puts("Inited");
+//	// Ìæ»»´°¿Ú¹ý³Ì
+//	originalWndProc = (WNDPROC)GetWindowLongPtr(hGameWnd, GWLP_WNDPROC);
+//	if (originalWndProc)
+//		SetWindowLongPtr(hGameWnd, GWLP_WNDPROC, (LONG_PTR)PatchedWndProc);
+//	_endthread();
+//}
+unsigned char* const ADDR_DINPUT_SETCOOPERATIVE_SPEC = (unsigned char*)0x40d8b4;
+inline void tamperDInputSetup() {
+	DWORD old;
+	VirtualProtect(ADDR_DINPUT_SETCOOPERATIVE_SPEC, 1, PAGE_WRITECOPY, &old);
+	printf("Original DInput SetCooperativeLevel spec: %#02X changed to ", *ADDR_DINPUT_SETCOOPERATIVE_SPEC);
+	*ADDR_DINPUT_SETCOOPERATIVE_SPEC = DISCL_FOREGROUND | DISCL_NONEXCLUSIVE;// | DISCL_NOWINKEY;
+	printf("%#02X\n", *ADDR_DINPUT_SETCOOPERATIVE_SPEC);
+	VirtualProtect(ADDR_DINPUT_SETCOOPERATIVE_SPEC, 1, old, &old);
+}
+
+
 extern "C" __declspec(dllexport) bool Initialize(HMODULE hMyModule, HMODULE hParentModule)
 {
-	DWORD old;
-
 #ifdef _DEBUG
 	FILE *_;
-
 	AllocConsole();
 	freopen_s(&_, "CONOUT$", "w", stdout);
 	freopen_s(&_, "CONOUT$", "w", stderr);
+	puts("Hello, world! by FixSokuWndProc");
 #endif
-
-	puts("Hello, world!");
-	VirtualProtect((PVOID)RDATA_SECTION_OFFSET, RDATA_SECTION_SIZE, PAGE_EXECUTE_WRITECOPY, &old);
-	ogBattleMgrOnRender  = SokuLib::TamperDword(&SokuLib::VTable_BattleManager.onRender,  CBattleManager_OnRender);
-	ogBattleMgrOnProcess = SokuLib::TamperDword(&SokuLib::VTable_BattleManager.onProcess, CBattleManager_OnProcess);
-	VirtualProtect((PVOID)RDATA_SECTION_OFFSET, RDATA_SECTION_SIZE, old, &old);
-
-	FlushInstructionCache(GetCurrentProcess(), nullptr, 0);
+	DWORD old;
+	//_beginthread(afterWindowStart, 0, nullptr);
+	orgUpdateWindow = TamperIndirectCall(0x7fb735, afterWndCreated);
+	
+	tamperDInputSetup();
+	
 	return true;
 }
 
 extern "C" int APIENTRY DllMain(HMODULE hModule, DWORD fdwReason, LPVOID lpReserved)
 {
-	return TRUE;
+	return true;
 }
 
-// New mod loader functions
-// Loading priority. Mods are loaded in order by ascending level of priority (the highest first).
-// When 2 mods define the same loading priority the loading order is undefined.
+//check Soku 1.10a
+extern "C" __declspec(dllexport) bool CheckVersion(const BYTE hash[16])
+{
+	return memcmp(hash, SokuLib::targetHash, sizeof(SokuLib::targetHash)) == 0;
+}
 extern "C" __declspec(dllexport) int getPriority()
 {
-	return 0;
+	return -1;
 }
-
-// Not yet implemented in the mod loader, subject to change
-// SokuModLoader::IValue **getConfig();
-// void freeConfig(SokuModLoader::IValue **v);
-// bool commitConfig(SokuModLoader::IValue *);
-// const char *getFailureReason();
-// bool hasChainedHooks();
-// void unHook();
